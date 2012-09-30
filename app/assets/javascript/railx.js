be510b46d1dc8ad3077ac4939618a26294c7fc43
Paolo <paolo@headwalk.net>
2012-10-01 00:30:52 +0200

    Renamed js files

--- app/assets/javascript/railx.js
+++ app/assets/javascript/railx.js
@@ -1,7 +1,7 @@
-//* require railx/01_jquery.form
-//* require railx/02_jquery.history
-//* require railx/03_jrails
-//* require railx/04_ajaxable
+//* require railx/jquery.form
+//* require railx/jquery.history
+//* require railx/ajaxable
+//* require railx/jrails
 //* require jquery.gritter
 
 console.log("montato 1")
